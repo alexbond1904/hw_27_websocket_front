8efd183198c30d9777e9ac0b4dea069af4a1ea6c
alexbond1904 <alexbond19@gmail.com>
2025-03-21 01:39:35 +0200

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,33 @@ import Name from "./components/Name.tsx";
 import {WebSocketContext} from "./utils/webSocketContext.ts";
 import {useState, useRef} from "react";
 import Chat from "./components/Chat.tsx";
+import Spinner from "./components/Spinner.tsx";
 
 export const URL_WEBSOCKET = 'ws://localhost:8080';
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true)
     const [isRegistered, setRegistered] = useState(false);
     const [userName, setUserName] = useState('');
+    const [online, setOnline] = useState(0)
+
 
     const webSocketRef = useRef<WebSocket | null>(null);
 
     if (!webSocketRef.current || webSocketRef.current.readyState === WebSocket.CLOSED) {
         webSocketRef.current = new WebSocket(URL_WEBSOCKET);
 
-        webSocketRef.current.onopen = () => console.log("Connected to server");
+        webSocketRef.current.onopen = () => {
+            console.log("Connected to server");
+            setIsLoading(false);
+        }
+
+        webSocketRef.current.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            if(data?.type === 'online') {
+            setOnline(data.count);
+            return;
+        }}
 
         webSocketRef.current.onclose = () => {
             console.log("WebSocket closed");
@@ -31,8 +45,9 @@ return (
             name: userName,
         }}>
             <div className={'max-w-[300px] text-center mx-auto mt-10'}>
+                {isLoading && <Spinner/>}
                 {!isRegistered && <Name setRegistered={setRegistered} setUserName={setUserName}/>}
-                {isRegistered && <Chat/>}
+                {isRegistered && <Chat online={online} setOnline={setOnline} />}
             </div>
         </WebSocketContext.Provider>
     ));
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -7,61 +7,68 @@ export interface Message {
     time: string
 }
 
-const Chat = () => {
+interface ChatProps {
+    online: number,
+    setOnline: (value: (((prevState: number) => number) | number)) => void
+}
+
+const Chat = ({online, setOnline}: ChatProps) => {
     const [message, setMessage] = useState('');
-    const [online, setOnline] = useState(0)
-    const { name, webSocket: ws  } = useContext(WebSocketContext);
+    const {name, webSocket: ws} = useContext(WebSocketContext);
     const [chatMessages, setChatMessages] = useState<Message[]>([]);
 
 
-    if(ws) ws.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        if (!data) return;
-        if(data?.type === 'online') {
-            setOnline(data.count);
-            return;
-        }
-        const message:Message = {
-            message: data.message,
-            author: data.name,
-            time: data.time
-        }
-        setChatMessages(prev => [message, ...prev]);
-    };
+    if (ws) {
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            if (!data) return;
+            if (data?.type === 'online') {
+                setOnline(data.count);
+                return;
+            }
+            const message: Message = {
+                message: data.message,
+                author: data.name,
+                time: data.time
+            }
+            setChatMessages(prev => [message, ...prev]);
+        };
+    }
 
     const handleClick = () => {
         ws?.send(JSON.stringify({
             type: "message",
             message: message
         }))
+        setMessage('');
     }
 
     return (
-            <div className={'grid gap-3'}>
-                <div className={'text-left'}><p className={'text-xl'}>Hello {name}!</p></div>
-                <textarea
-                    value={message}
-                    onChange={(e) => setMessage(e.currentTarget.value)}
-                    className={'border border-gray-400 p-3 rounded-md'}
-                    placeholder={'Your message'}>
+        <div className={'grid gap-3'}>
+            <div className={'text-left'}><p className={'text-xl'}>Hello {name}!</p></div>
+            <textarea
+                value={message}
+                onChange={(e) => setMessage(e.currentTarget.value)}
+                className={'border border-gray-400 p-3 rounded-md'}
+                placeholder={'Your message'}>
                 </textarea>
-                <button onClick={handleClick} className={'bg-blue-500 p-3 text-white rounded-md cursor-pointer'}>Send
-                </button>
-                <p>Chat. Online: <span className={'text-orange-500'}><u>{online}</u></span></p>
-                {!!chatMessages.length && chatMessages.map((message, index) => (
-                    <div key={index} className="text-left p-2 ">
-                        <p className={'text-xs text-gray-400'}>{message.time}</p>
-                        <p className={'text-md text-gray-600'}>
-                            <span className={`font-bold ${message.author == name ? 'text-green-600': 'text-black'}`}>
+            <button onClick={handleClick} className={'bg-blue-500 p-3 text-white rounded-md cursor-pointer'}>Send
+            </button>
+            <p>Chat. Online: <span className={'text-orange-500'}>{online}</span></p>
+            {!!chatMessages.length && chatMessages.map((message, index) => (
+                <div key={index} className="text-left p-2 ">
+                    <p className={'text-xs text-gray-400'}>{message.time}</p>
+                    <p className={'text-md text-gray-600'}>
+                            <span className={`font-bold ${message.author == name ? 'text-green-600' : 'text-black'}`}>
                                {message.author == name ? 'You' : message.author}:&nbsp;
                             </span>
-                            {message.message}
-                        </p>
-                        <hr className={'mt-2 text-gray-300'}/>
-                    </div>
+                        {message.message}
+                    </p>
+                    <hr className={'mt-2 text-gray-300'}/>
+                </div>
 
-                ))}
-            </div>
+            ))}
+        </div>
     );
 };
 
